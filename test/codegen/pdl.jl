40b04532485697ba3611fb739d4168a11be701ea
Tim Besard <tim.besard@gmail.com>
2026-08-10 23:17:19 +0200

    Gate test.

--- test/codegen/pdl.jl
+++ test/codegen/pdl.jl
@@ -2,37 +2,39 @@ spec1d = ct.ArraySpec{1}(16, true, (0,), (16,))
 AT = ct.TileArray{Float32, 1, Int32, spec1d}
 
 @testset "programmatic dependent launch" begin
-    @test @filecheck begin
-        @check "[[STORE:%.+]] = store_view_tko"
-        @check "[[JOIN:%.+]] = join_tokens{{.*}}[[STORE]]"
-        @check "gdc_launch_dependents_tko token = [[JOIN]]"
-        code_tiled(Tuple{AT, AT}; bytecode_version=v"13.4") do a, b
-            ct.store(b, 1, ct.load(a, 1, (16,)))
-            ct.grid_dependency_control_launch_dependents()
-            return
-        end
-    end
-
-    @test @filecheck begin
-        @check "[[WAIT:%.+]] = gdc_wait_tko"
-        @check "[[JOIN:%.+]] = join_tokens{{.*}}[[WAIT]]"
-        @check "load_view_tko{{.*}} token = [[JOIN]]"
-        code_tiled(Tuple{AT, AT, AT}; bytecode_version=v"13.4") do a, b, c
-            Base.donotdelete(ct.load(a, 1, (16,)))
-            ct.grid_dependency_control_wait()
-            ct.store(c, 1, ct.load(b, 1, (16,)))
-            return
-        end
-    end
-
-    @test @filecheck begin
-        @check "if"
-        @check "gdc_wait_tko"
-        code_tiled(Tuple{Bool}; bytecode_version=v"13.4") do condition
-            if condition
-                ct.grid_dependency_control_wait()
+    if ct.tileir_disassembler_version() >= v"13.4"
+        @test @filecheck begin
+            @check "[[STORE:%.+]] = store_view_tko"
+            @check "[[JOIN:%.+]] = join_tokens{{.*}}[[STORE]]"
+            @check "gdc_launch_dependents_tko token = [[JOIN]]"
+            code_tiled(Tuple{AT, AT}; bytecode_version=v"13.4") do a, b
+                ct.store(b, 1, ct.load(a, 1, (16,)))
+                ct.grid_dependency_control_launch_dependents()
+                return
             end
-            return
+        end
+
+        @test @filecheck begin
+            @check "[[WAIT:%.+]] = gdc_wait_tko"
+            @check "[[JOIN:%.+]] = join_tokens{{.*}}[[WAIT]]"
+            @check "load_view_tko{{.*}} token = [[JOIN]]"
+            code_tiled(Tuple{AT, AT, AT}; bytecode_version=v"13.4") do a, b, c
+                Base.donotdelete(ct.load(a, 1, (16,)))
+                ct.grid_dependency_control_wait()
+                ct.store(c, 1, ct.load(b, 1, (16,)))
+                return
+            end
+        end
+
+        @test @filecheck begin
+            @check "if"
+            @check "gdc_wait_tko"
+            code_tiled(Tuple{Bool}; bytecode_version=v"13.4") do condition
+                if condition
+                    ct.grid_dependency_control_wait()
+                end
+                return
+            end
         end
     end
 
